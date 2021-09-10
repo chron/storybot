0ec98652dd3a2ae3908981569d424063207b2b7f
Paul Prestidge <paul@storypark.com>
2021-09-10 15:30:43 +1200

    Update standup message

--- src/shared/standup.js
+++ src/shared/standup.js
@@ -7,12 +7,12 @@ module.exports = function standup(users, userNames) {
 They'll be responsible for facilitating our standup meeting at 9.15.
 If they aren't available, the backup sloth is :sloth: ${userNames[backup]} :sloth:
 
-:jira::jira::jira: If you are facilitating, please bring your laptop to present the Jira board. :jira::jira::jira:
-
 *1.* What did you do yesterday? What is your plan for today?
 *2.* Anything you need help with?  Do you have capacity to help others?
 *3.* Any updates for the team?
 *4.* _<question of the day>_ (if there's time :grimacing:)
 
-If you need ideas for a question of the day, <https://andyhansen.co.nz/posts/stand-up-bonus-question|this> is a great list.`;
+If you need ideas for a question of the day, <https://andyhansen.co.nz/posts/stand-up-bonus-question|this> is a great list.
+
+You can see an up-to-date list of our in-flight PRs at <https://sloth.engineering/today/|sloth.engineering>, or use the \`/prs\` slash command.`;
 }
