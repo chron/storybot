49dfd6d29f78c5805eb429b2eb62a73286b7b171
Paul Prestidge <paul@storypark.com>
2021-11-22 11:51:38 +1300

    Allow individual day exceptions for standups, too

--- src/shared/standDown.js
+++ src/shared/standDown.js
@@ -3,6 +3,8 @@ const formatTz = require('date-fns-tz/format');
 
 const specialCases = [
   ['2021-05-27', "No stand-down today — we're in the middle of a retro! Hope it's going well."],
+  ['2021-11-22', "No stand-down today — we're in the middle of a retro! Hope it's going well."],
+  ['2021-11-25', "No stand-down today — we're still in an all day team thing!"],
 ];
 
 module.exports = function standDown(team, isItOnToday) {
--- src/shared/standup.js
+++ src/shared/standup.js
@@ -1,8 +1,18 @@
 const shuffle = require('@architect/shared/shuffle');
+const utcToZonedTime = require('date-fns-tz/utcToZonedTime');
+const formatTz = require('date-fns-tz/format');
+
+const specialCases = [
+  ['2021-11-25', "No standup today — there's some kind of all team offsite thing!"],
+];
 
 module.exports = function standup(team, userNames) {
   if (!team.standupCaptains) { return null; }
   if (!team.standupMessage) { return null; }
+
+  const dateInNZST = utcToZonedTime(new Date(), 'Pacific/Auckland');
+  const c = specialCases.find(([d]) => d === formatTz(dateInNZST, 'yyyy-MM-dd'));
+  if (c) { return c[1]; }
 
   const [main, backup] = shuffle(team.standupCaptains);
 
