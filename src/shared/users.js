c55202f9be406005432f10133db222310e95c228
timothyjohn2015 <tim@storypark.com>
2023-10-30 07:48:32 +1300

    Remove old staff

--- src/shared/users.js
+++ src/shared/users.js
@@ -7,13 +7,10 @@
 
 module.exports.users = {
   libbyschuknight: '<@URA1E530R>', // Libby
-  'louisa-storypark': '<@UB544UWJZ>', // Louisa
   marielleaurabelle: '<@U017PV0FEJJ>', // Marielle
   timothyjohn2015: '<@U0CPRJGG2>', // Tim
-  fourseven: '<@U0431G60W>', // Mathew
   AlexQuinlivan: '<@U08R7BSLR>', // Alex
   AaronThornton00: '<@U048FJY1K>', // Aaron
-  'Rob-Bee-Neilson': '<@UPE3KH494>', // Robbie
   StoryparkNick: '<@U03TVGDFWF9>', // Nick
   'opal-storypark': '<@U047FJT9SKZ>', // Opal
 
@@ -22,13 +19,10 @@ module.exports.users = {
   shannon: '<@U01N1BGP7K6>', // Shannon
   andrew: '<@U06286ZSM>', // Andrew
   sarah: '<@UKWUY75RD>', // Sarah
-  madeline: '<@U02BYBQ8BM5>', // Madeline
-  taylor: '<@U02JN7VC3EF>', // Taylor
   shalita: '<@U01THA12EET>', // Shalita
   kerry: '<@U01UP5F2G91>',  // Kerry
   eileen: '<@ULHKU7MQW>',  // Eileen
   dave: '<@U01KNSCGVFW>',  // Dave Keyes
-  charlotte: '<@U02BZ4Z9DRD>', // Charlotte
   nat: '<@U039RN8T97E>', // Nat
   geena: '<@U03EH7JELTT>', // Geena
   david: '<@U04BAC97XPU>', // David
